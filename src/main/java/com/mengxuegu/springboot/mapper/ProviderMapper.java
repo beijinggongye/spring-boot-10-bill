cd3db415921dd1a1484dc00b3c7e8ef553216e2a
zhang <zhang@zhangdeMacBook-Pro.local>
2019-06-09 22:24:40 +0800

    Initial commit

---
+++ src/main/java/com/mengxuegu/springboot/mapper/ProviderMapper.java
@@ -0,0 +1,24 @@
+package com.mengxuegu.springboot.mapper;
+
+import com.mengxuegu.springboot.entities.Provider;
+
+
+import java.util.List;
+
+/**
+ * @Auther: zhang
+ */
+//@Mapper 或 @MapperScan("com.mengxuegu.springboot.mapper")
+public interface ProviderMapper {
+
+    List<Provider> getProviders(Provider provider);
+
+    Provider getProviderByPid(Integer pid);
+
+    int addProvider(Provider provider);
+
+    int deleteProviderByPid(Integer pid);
+
+    int updateProvider(Provider provider);
+
+}
